d2088e6980be1cccab7f7c4850b30abe663fce6d
Gol3vka <gol3vka@163.com>
2022-11-03 23:57:13 +0800

    Feat | server! web page!

--- find_chargers.py
+++ find_chargers.py
@@ -2,18 +2,20 @@
 # -*- coding:utf-8 -*-
 '''Where-Are-Available-Chargers:
     希望这个脚本可以帮助你更容易给小电驴找到充电桩。
+@File: find_chargers.py
+@Brief: 使用 requests 爬取充电桩信息，返回字典或字符串类型数据。
 @Author: Golevka2001<gol3vka@163.com>
-@Version: 1.1.0
+@Version: 2.0.0
 @Created Date: 2022/11/01
 @Last Modified Date: 2022/11/03
 '''
 
 # TODO: 现在只存了少数几个充电桩的码，还有很多二维码需要之后添加进来
-# TODO: 之后会想一个更方便查询的方法，比如邮件、网页什么的
 
 import requests
 import yaml
 import os
+import time
 
 
 class FindChargers:
@@ -40,6 +42,14 @@ class FindChargers:
                                  headers=self.config['headers'],
                                  allow_redirects=False).json()
         self.token = response['data']['token']
+
+    def get_time(self) -> str:
+        '''Return current time(used for the web page)
+
+        Returns:
+            str: current time
+        '''
+        return time.strftime("%Y-%m-%d %H:%M:%S")
 
     def where_are_you(self):
         '''Traverse all the charging stations in the list
@@ -73,7 +83,7 @@ class FindChargers:
         # delete the authorization key:
         del [self.config['headers']['authorization']]
 
-    def tell_me(self, type: str) -> dict:
+    def tell_me(self, type: str):
         '''Return different result using different line break character
 
         Args:
@@ -99,4 +109,4 @@ if __name__ == '__main__':
                                'config.yml')
     test = FindChargers(config_path)
     test.where_are_you()
-    print(test.tell_me('html'))
+    print(test.tell_me('str'))
--- server.py
+++ server.py
@@ -1,16 +1,38 @@
+#!user/bin/env python
+# -*- coding:utf-8 -*-
+'''Where-Are-Available-Chargers:
+    希望这个脚本可以帮助你更容易给小电驴找到充电桩。
+@File: find_chargers.py
+@Brief: 使用 flask 框架搭建的简单服务，将爬取的信息显示在网页上。
+@Author: Golevka2001<gol3vka@163.com>
+@Version: 2.0.0
+@Created Date: 2022/11/01
+@Last Modified Date: 2022/11/03
+'''
+
 from flask import Flask, render_template
 from find_chargers import FindChargers
+import os
 
 app = Flask(__name__)
 
 
+# page & pass result in
 @app.route("/")
 def index():
-    chargers = FindChargers()
+    config_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               'config.yml')
+    chargers = FindChargers(config_path)
     chargers.where_are_you()
     result = chargers.tell_me('html')
-    return render_template('index.html', east_gate=result['east_gate'])
+    last_time = chargers.get_time()
+    return render_template('index.html',
+                           east_gate=result['东门'],
+                           west_gate=result['西门'],
+                           last_time=last_time)
 
 
+# run server
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=1205, debug=False)
+    while True:
+        app.run(host='0.0.0.0', port=1234, debug=False)
