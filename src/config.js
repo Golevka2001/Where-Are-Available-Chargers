0e294e69cd1803cb7629c067b5d11bce8b79e398
csimide <117500413+csimide@users.noreply.github.com>
2023-04-09 20:59:38 +0800

    Ver 3.4.0 | 完善北门东侧B

--- src/config.js
+++ src/config.js
@@ -30,7 +30,7 @@ const CONFIG = {
         "南门西侧": ["XY679RB8P35Y10", "XYOGaVYy7OHu7"],
         //"南门东侧": ["XY1eANYJoWM620", "XYlFl6DnfgZH3"],
         "北门东侧A": ["XYeo2IsT1Sx61", "XYKCwwkZXBW62", "XYTbok829tYo8", "XYo7UstSCeu64"],
-        "北门东侧B": [,,"XYnT1V47gbvQ5","XYwgYSUv41Ve2","XYG9NcgUXBlg4",]
+        "北门东侧B": ["XYbwFHY40PZK3","XYgbwesjiMEy6","XYnT1V47gbvQ5","XYwgYSUv41Ve2","XYWOjzU06jWc1","XYG9NcgUXBlg4"]
     },
     "stations_r": {
         "XYlFl6DnfgZH3": { station: "东门北侧", station_EN: "SE", num: 0 },
@@ -54,9 +54,12 @@ const CONFIG = {
         "XYKCwwkZXBW62": { station: "北门东侧A", station_EN: "NEA", num: 1 },
         "XYTbok829tYo8": { station: "北门东侧A", station_EN: "NEA", num: 2 },
         "XYo7UstSCeu64": { station: "北门东侧A", station_EN: "NEA", num: 3 },
+        "XYbwFHY40PZK3": { station: "北门东侧B", station_EN: "NEB", num: 0 },
+        "XYgbwesjiMEy6": { station: "北门东侧B", station_EN: "NEB", num: 1 },
         "XYnT1V47gbvQ5": { station: "北门东侧B", station_EN: "NEB", num: 2 },
         "XYwgYSUv41Ve2": { station: "北门东侧B", station_EN: "NEB", num: 3 },
-        "XYG9NcgUXBlg4": { station: "北门东侧B", station_EN: "NEB", num: 4 }
+        "XYWOjzU06jWc1": { station: "北门东侧B", station_EN: "NEB", num: 4 },
+        "XYG9NcgUXBlg4": { station: "北门东侧B", station_EN: "NEB", num: 5 },
     },
     "station_ENs": {
         "东门北侧": "EN",
