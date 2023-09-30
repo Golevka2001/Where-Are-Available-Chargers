7370b89468a2cfa0863c4229ff2ed9fb50678cb6
csimide <117500413+csimide@users.noreply.github.com>
2023-09-30 11:12:00 +0800

    Fix | Use relative path for `apiUrl`
    …in File `chargers-status\index.ts`

--- src/apis/chargers-status/index.ts
+++ src/apis/chargers-status/index.ts
@@ -6,14 +6,7 @@ export const getChargersStatus = async (): Promise<{
   update_message: UpdateMessage;
   status_detail: StatusDetail;
 }> => {
-  let apiUrl = '';
-  if (process.env.NODE_ENV === 'development') {
-    apiUrl = 'http://127.0.0.1:3000/api/get_status';
-  }
-  // TODO：生产环境记得修改为后端提供的接口地址
-  else {
-    apiUrl = '【接口地址】';
-  }
+  const apiUrl = process.env.API_URL || 'api/get_status';
   const res = await axios.get(apiUrl);
   // TODO：根据后端返回的格式相应调整
   return res.data.testData;
