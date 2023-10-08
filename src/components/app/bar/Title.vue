65e665107aec732e4f2d62ed2673d5fd537f6cbf
Gol3vka <gol3vka@163.com>
2023-10-08 16:13:23 +0800

    Feat | 川剧变脸

--- src/components/app/bar/Title.vue
+++ src/components/app/bar/Title.vue
@@ -1,14 +1,46 @@
 <!-- 顶部导航栏标题 -->
 
 <template>
-  <v-app-bar-title class="mx-0 text-center">
-    <span> &#128268;&#128545; </span>
+  <v-app-bar-title
+    class="mx-0 text-center"
+    style="user-select: none"
+  >
+    <span>&#128268;</span>
+    <!-- 川剧变脸 -->
+    <v-scroll-y-reverse-transition leave-absolute>
+      <span
+        @click="clickEmojiFace"
+        :key="emojiFace"
+        v-html="emojiFace"
+        style="cursor: pointer"
+      ></span>
+    </v-scroll-y-reverse-transition>
     <router-link
       to="/"
       class="font-weight-medium text-decoration-none text-no-wrap"
       style="font-size: 1rem; color: inherit"
     >
-      Where Are Available Chargers
+      &nbsp;Where Are Available Chargers
     </router-link>
   </v-app-bar-title>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const emojiFace = ref('&#128545;');
+
+const clickEmojiFace = () => {
+  // 黄豆和小动物脸的编码范围
+  const emojiCodeRangeList = [
+    [128045, 128060],
+    [128512, 128580],
+    [129296, 129303],
+    [129312, 129327],
+  ];
+  const rangeIndex = Math.floor(Math.random() * emojiCodeRangeList.length);
+  const [start, end] = emojiCodeRangeList[rangeIndex];
+  const emojiCode = Math.floor(Math.random() * (end - start + 1)) + start;
+  emojiFace.value = `&#${emojiCode};`;
+};
+</script>
