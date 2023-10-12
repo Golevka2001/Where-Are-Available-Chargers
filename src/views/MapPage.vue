ef38d851bf30ce9c3022b7e6d10f8bedc4ac3437
Gol3vka <gol3vka@163.com>
2023-10-12 21:24:50 +0800

    Fix | Adjust the display of `map`

--- src/views/MapPage.vue
+++ src/views/MapPage.vue
@@ -1,36 +1,39 @@
 <!-- 充电桩位置示意图页面 -->
 
 <template>
-  <div
-    v-vue-viewer.static="{
-      backdrop: false,
-      fullscreen: false,
-      initialCoverage: '1',
-      inline: true,
-      navbar: false,
-      title: false,
-      toggleOnDblclick: false,
-      toolbar: {
-        zoomIn: 'large',
-        zoomOut: 'large',
-        oneToOne: 'large',
-        reset: 'large',
-        prev: 0,
-        play: 0,
-        next: 0,
-        rotateLeft: 0,
-        rotateRight: 0,
-        flipHorizontal: 0,
-        flipVertical: 0,
-      },
-    }"
-    class="text-center"
-  >
-    <img
-      :src="config.latestMapPath"
-      alt="充电桩位置示意图"
-      width="100%"
-    />
+  <div class="fill-height">
+    <div
+      v-vue-viewer.static="{
+        backdrop: false,
+        button: false,
+        fullscreen: false,
+        initialCoverage: 1,
+        inline: true,
+        navbar: false,
+        title: false,
+        toggleOnDblclick: false,
+        toolbar: {
+          zoomIn: 'large',
+          zoomOut: 'large',
+          oneToOne: 'large',
+          reset: 'large',
+          prev: 0,
+          play: 0,
+          next: 0,
+          rotateLeft: 0,
+          rotateRight: 0,
+          flipHorizontal: 0,
+          flipVertical: 0,
+        },
+      }"
+      class="text-center"
+    >
+      <img
+        :src="config.latestMapPath"
+        alt="充电桩位置示意图"
+        width="100%"
+      />
+    </div>
   </div>
 </template>
 
