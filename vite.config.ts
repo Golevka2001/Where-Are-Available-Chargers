86b314d002f812ec212b7bd109ae607d7ecffd01
csimide <117500413+csimide@users.noreply.github.com>
2023-10-21 21:05:15 +0800

    Feat | Support for PWA and Service Worker

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@
 
 import vue from '@vitejs/plugin-vue';
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
-import ViteFonts from 'unplugin-fonts/vite';
+// import ViteFonts from 'unplugin-fonts/vite';
 import { viteMockServe } from 'vite-plugin-mock';
 
 import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
+
+import { VitePWA } from 'vite-plugin-pwa';
+// Generate PWA/Service Worker files
 
 export default defineConfig({
   plugins: [
@@ -16,7 +19,7 @@ export default defineConfig({
     vuetify({
       autoImport: true,
     }),
-    ViteFonts({
+    /*ViteFonts({
       google: {
         families: [
           {
@@ -25,10 +28,39 @@ export default defineConfig({
           },
         ],
       },
-    }),
+    }),*/
+    // 似乎没有必要引入字体，先注释掉了
     viteMockServe({
       mockPath: 'mock',
       enable: process.env.NODE_ENV === 'development',
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Where Are Available Chargers?!',
+        short_name: 'Chargers',
+        description: 'Find Chargers Easily in Southeast University',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa/maskable-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
     }),
   ],
   define: { 'process.env': {} },
