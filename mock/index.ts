8a36c99688c980b9c7da87163ae409ff8860a515
Gol3vka <gol3vka@163.com>
2023-10-23 22:02:03 +0800

    Feat | Add headers for `mock` responses

--- mock/index.ts
+++ mock/index.ts
@@ -1,3 +1,5 @@
+import { MockMethod } from 'vite-plugin-mock';
+
 import { genRandomStatusData } from './status-gen';
 import { genRandomWeatherData } from './weather-gen';
 
@@ -7,8 +9,10 @@ export default [
     method: 'get',
     statusCode: 200,
     timeout: Math.floor(Math.random() * 1000) + 500,
-    response: () => {
-      return genRandomStatusData();
+    rawResponse: (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end(JSON.stringify(genRandomStatusData()));
     },
   },
   {
@@ -16,8 +20,10 @@ export default [
     method: 'get',
     statusCode: 200,
     timeout: Math.floor(Math.random() * 1000) + 500,
-    response: () => {
-      return genRandomWeatherData();
+    rawResponse: (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end(JSON.stringify(genRandomWeatherData()));
     },
   },
-];
+] as MockMethod[];
