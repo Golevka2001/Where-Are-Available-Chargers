48b22d225d7ba41ebbd6467d91e2281da0b6914a
csimide <117500413+csimide@users.noreply.github.com>
2023-10-24 18:16:31 +0800

    Revert "Fix | Try to fix `challenge` page"
    This reverts commit 8d1384a5b1291172f467801d8a9c943d0dfe03d7.

--- src/apis/charger.ts
+++ src/apis/charger.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import config from '@/config';
 import { StatusResponse } from '@/types/charger';
+import router from '@/router';
 
 export const getChargersStatus = async (): Promise<StatusResponse> => {
   const res = await axios
@@ -10,12 +11,16 @@ export const getChargersStatus = async (): Promise<StatusResponse> => {
     .catch((err) => {
       // 先处理质询
       if (
+        err.response &&
         err.response.status === 403 &&
         err.response.headers['cf-mitigated'] === 'challenge'
       ) {
-        // 如果用 router.push 有很奇怪的 bug，不会修，所以用 window.location.href
-        window.location.href =
-          '/challenge?' + encodeURIComponent(window.location.pathname);
+        router.push({
+          path: '/challenge',
+          query: {
+            callback: router.currentRoute.value.fullPath,
+          },
+        });
       } else throw new Error('充电桩状态请求失败：' + err.message);
     });
   if (!res.data) return { last_update_time: 0, status: undefined, code: 403 };
